8db56c7c7ec5261a0e12f6225a733c36e0669592
Ле Чонг Минь <59441846+letrongminh@users.noreply.github.com>
2020-10-20 21:43:32 +0300

    fix PEP 8

--- the_second_way.py
+++ the_second_way.py
@@ -5,6 +5,8 @@ spaces = 0
 punctuation_marks = 0
 number_of_sentences = 0
 words = 0
+
+
 try:
     open_file = open("steam_description_data.csv","r", encoding="utf-8")
     csv_reader = csv.reader(open_file)
@@ -12,7 +14,10 @@ try:
         characters = ",".join(line)
         all_char += len(characters)
         spaces += characters.count(" ")
-        punctuation_marks += characters.count(".") + characters.count(",") + characters.count("?") + characters.count("(") + characters.count(")") + characters.count(";") + characters.count("-") + characters.count("_") + characters.count(":") + characters.count("/") + characters.count("\'") + characters.count("\"")
+        punctuation_marks += characters.count(".") + characters.count(",") + characters.count("?") \
+                + characters.count("(") + characters.count(")") + characters.count(";") \
+                + characters.count("-") + characters.count("_") + characters.count(":") \
+                + characters.count("/") + characters.count("\'") + characters.count("\""))
         words += len(re.findall(r"(\w+'\w+)|(\w+-\w+'\w+)|(\w+-\w+'\w)|\w+", characters))
         number_of_sentences += len(re.findall(r"([A-Z][^\.!?]*[\.!?])", characters))
     print("Total number of characters:", all_char)
